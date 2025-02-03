4d97668e77027742b77e3871141a0fb086f2eff2
Saurav <sukumarsaurav@gmail.com>
2025-02-04 03:45:01 +0530

    init db

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'weblink_db';
+$db_host = '193.203.184.121';
+$db_user = 'u911550082_webase';
+$db_pass = 'z:qHAizSy4#';
+$db_name = 'u911550082_webase';
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
