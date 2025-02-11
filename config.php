e6ff14c34457ca80521937d9206ceb099f57c185
Saurav <sukumarsaurav@gmail.com>
2025-02-12 01:54:41 +0530

    style

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$db_host = '193.203.184.121';
+$db_host = 'localhost';
 $db_user = 'u462549710_admin';
-$db_pass = '1N+FOyto';
+$db_pass = '!Apnt^&d5';
 $db_name = 'u462549710_ssinfotechweb';
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
